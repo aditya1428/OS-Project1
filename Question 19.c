0f68ca64c3245c03ba555297d5bbdbf8cd90b3db
Aditya Kumar <32673122+aditya1428@users.noreply.github.com>
2018-04-16 11:08:39 +0530

    Update Question 19.c

--- Question 19.c
+++ Question 19.c
@@ -14,3 +14,20 @@ main()
 	i[j].s='s';
 	i[j].n=j+1;
 	}
+	printf("***********************Ramdon arrangement of gifts taken by students**************************\n");
+	for(j=0;j<10;j++)
+	{
+	printf("\nEnter the number of gifts taken by Student %c%d  : ",i[j].s,i[j].n,"\n\n");
+	scanf("%d",&i[j].g);
+	}
+	int k,temp;
+	for(k=0;k<9;k++)
+	for(j=0;j<9-k;j++)
+	{
+		if(i[j].g<i[j+1].g)
+		{
+			temp=i[j].g;
+			i[j].g=i[j+1].g;
+			i[j+1].g=temp;
+		}
+	}
